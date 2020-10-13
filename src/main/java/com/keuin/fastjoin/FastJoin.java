6360368caed0931ef89490d6256e316417778843
Keuin <keuinx@gmail.com>
2020-10-13 15:21:14 +0800

    Initial version (1.0)

--- src/main/java/com/keuin/fastjoin/FastJoin.java
+++ src/main/java/com/keuin/fastjoin/FastJoin.java
@@ -1,13 +1,18 @@
 package com.keuin.fastjoin;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class FastJoin implements ModInitializer {
+
+	private static final Logger LOGGER = LogManager.getLogger();
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		System.out.println("FastJoin is enabled. If you have any issue, please report it at https://github.com/keuin/fastjoin");
+		LOGGER.info("FastJoin is enabled. If you have any issue, please report it at https://github.com/keuin/fastjoin");
 	}
 }
